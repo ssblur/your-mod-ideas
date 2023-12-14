761b4a4546555702451be3f08b610f3eec762097
Patrick Hallbick <patrick.hallbick@outlook.com>
2023-12-14 09:57:46 -0500

    Update to 1.20.4

--- common/src/main/java/com/ssblur/yourmodideas/blocks/FurnitureBlock.java
+++ common/src/main/java/com/ssblur/yourmodideas/blocks/FurnitureBlock.java
@@ -1,24 +1,30 @@
 package com.ssblur.yourmodideas.blocks;
 
+import com.mojang.serialization.MapCodec;
 import dev.architectury.platform.Platform;
 import dev.architectury.registry.client.rendering.RenderTypeRegistry;
 import net.fabricmc.api.EnvType;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.GrassBlock;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 
 public class FurnitureBlock extends HorizontalDirectionalBlock {
+  public static final MapCodec<FurnitureBlock> CODEC = simpleCodec(FurnitureBlock::new);
+
   public FurnitureBlock(Properties properties) {
     super(properties.noOcclusion());
     this.registerDefaultState(defaultBlockState().setValue(FACING, Direction.NORTH));
 
-    if(Platform.getEnv() == EnvType.CLIENT && !Platform.isForge())
+    if(Platform.getEnv() == EnvType.CLIENT && !Platform.isForgeLike())
       RenderTypeRegistry.register(RenderType.cutout(), this);
+  }
+
+  @Override
+  protected MapCodec<? extends HorizontalDirectionalBlock> codec() {
+    return CODEC;
   }
 
   public BlockState getStateForPlacement(BlockPlaceContext blockPlaceContext) {
--- common/src/main/java/com/ssblur/yourmodideas/blocks/YourModIdeasBlocks.java
+++ common/src/main/java/com/ssblur/yourmodideas/blocks/YourModIdeasBlocks.java
@@ -14,7 +14,7 @@ public class YourModIdeasBlocks {
   public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(MOD_ID, Registries.BLOCK);
 
   public static final RegistrySupplier<Block> FROGGY_CHAIR = BLOCKS.register("froggy_chair", () ->
-    new FurnitureBlock(BlockBehaviour.Properties.copy(Blocks.ACACIA_PLANKS)));
+    new FurnitureBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.ACACIA_PLANKS)));
 
   public static void register() {
     BLOCKS.register();
